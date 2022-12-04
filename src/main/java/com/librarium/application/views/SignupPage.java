cd082bb50cff4cfa70143a84ab6e3464397f4783
AndreaCremonesi4 <a.cremonesi4@studenti.unibg.it>
2022-12-04 16:24:34 +0100

    Create SignupPage.java

---
+++ src/main/java/com/librarium/application/views/SignupPage.java
@@ -0,0 +1,13 @@
+package com.librarium.application.views;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("/signup")
+public class SignupPage extends VerticalLayout{
+	
+	public SignupPage() {
+		
+	}
+	
+}
